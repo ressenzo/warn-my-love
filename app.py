142efaf7a86f86cb7c04a735910da97d57861fa3
Renan Cossenzo <renancossenzo@hotmail.com>
2026-06-30 17:16:48 -0300

    fix: change execution time and remove comments (#3)

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ from src import llm, notification
 load_dotenv()
 
 def main():
-    # response = llm.ask_llm()
-    # if response.has_game_today:
-    notification.send_notification()
-    # else:
-    #     print("there is no game today")
+    response = llm.ask_llm()
+    if response.has_game_today:
+        notification.send_notification()
+    else:
+        print("there is no game today")
 
 if __name__ == "__main__":
     print("starting application...")
